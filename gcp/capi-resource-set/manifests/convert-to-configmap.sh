f68e2423ea28d8662d364b86566d94a04a8b9956
Cristiano Colangelo <cristiano@helio.exchange>
2022-09-20 17:39:26 +0200

    Reorganize a bit

--- gcp/capi-resource-set/manifests/convert-to-configmap.sh
+++ gcp/capi-resource-set/manifests/convert-to-configmap.sh
@@ -1,5 +1,5 @@
-kubectl create configmap calico-configmap --from-file=resource-original-definition/calico.yaml -o yaml --dry-run=client > calico-configmap.yaml
-kubectl create configmap custom-metrics-configmap --from-file=resource-original-definition/custom-metrics-apiserver.yaml -o yaml --dry-run=client > custom-metrics-configmap.yaml
-kubectl create configmap prometheus-configmap --from-file=resource-original-definition/prometheus.yaml -o yaml --dry-run=client > prometheus-configmap.yaml
-kubectl create configmap prometheus-node-exporter-configmap --from-file=resource-original-definition/prometheus-node-exporter.yaml -o yaml --dry-run=client > prometheus-node-exporter-configmap.yaml
-kubectl create configmap scheduler-extender-configmap --from-file=resource-original-definition/extender.yaml -o yaml --dry-run=client > scheduler-extender-configmap.yaml
+kubectl create configmap calico-configmap --from-file=original-manifests/calico.yaml -o yaml --dry-run=client > calico-configmap.yaml
+kubectl create configmap custom-metrics-configmap --from-file=original-manifests/custom-metrics-apiserver.yaml -o yaml --dry-run=client > custom-metrics-configmap.yaml
+kubectl create configmap prometheus-configmap --from-file=original-manifests/prometheus.yaml -o yaml --dry-run=client > prometheus-configmap.yaml
+kubectl create configmap prometheus-node-exporter-configmap --from-file=original-manifests/prometheus-node-exporter.yaml -o yaml --dry-run=client > prometheus-node-exporter-configmap.yaml
+kubectl create configmap scheduler-extender-configmap --from-file=original-manifests/extender.yaml -o yaml --dry-run=client > scheduler-extender-configmap.yaml
