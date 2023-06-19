51f45a4b70bf4eca7c4709f08099a6a2a04e4cc4
Cristiano Colangelo <cristiano@helio.exchange>
2023-06-19 14:46:14 +0200

    Minor changes

--- docker-tilt/templates/crs/convert-to-configmap.sh
+++ docker-tilt/templates/crs/convert-to-configmap.sh
@@ -3,6 +3,6 @@ kubectl create configmap custom-metrics-configmap --from-file=original-manifests
 kubectl create configmap custom-metrics-tls-secret-configmap --from-file=original-manifests/custom-metrics-tls-secret.yaml -o yaml --dry-run=client > custom-metrics-tls-secret-configmap.yaml
 kubectl create configmap prometheus-configmap --from-file=original-manifests/prometheus.yaml -o yaml --dry-run=client > prometheus-configmap.yaml
 kubectl create configmap prometheus-node-exporter-configmap --from-file=original-manifests/prometheus-node-exporter.yaml -o yaml --dry-run=client > prometheus-node-exporter-configmap.yaml
-kubectl create configmap scheduler-extender-configmap --from-file=original-manifests/extender.yaml -o yaml --dry-run=client > scheduler-extender-configmap.yaml
+kubectl create configmap extender-configmap --from-file=original-manifests/extender.yaml -o yaml --dry-run=client > extender-configmap.yaml
 kubectl create configmap tas-tls-secret-configmap --from-file=original-manifests/tas-tls-secret.yaml -o yaml --dry-run=client > tas-tls-secret-configmap.yaml
 kubectl create configmap tas-configmap --from-file=original-manifests/tas.yaml -o yaml --dry-run=client > tas-configmap.yaml
